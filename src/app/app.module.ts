c523e6473433bf4003af83601fa12fa7017e0ad1
MJWORKS <mjworks2020@outlook.com>
2022-06-15 20:35:54 -0300

    Created a basicHighlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from '../shared/directives/basic-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BasicHighlightDirective],
   imports: [CommonModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
