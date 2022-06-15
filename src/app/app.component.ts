c9c66ca4e0cda654ba3f72a6796df1594e8518dd
MJWORKS <mjworks2020@outlook.com>
2022-06-15 20:16:41 -0300

    Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'directives-examples';
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
