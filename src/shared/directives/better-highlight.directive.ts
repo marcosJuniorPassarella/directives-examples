569f587278ce90a304ecfab0f4e9dc40c2250537
MJWORKS <mjworks2020@outlook.com>
2022-06-16 10:29:52 -0300

    Transform a generic directive

--- src/shared/directives/better-highlight.directive.ts
+++ src/shared/directives/better-highlight.directive.ts
@@ -5,23 +5,28 @@ import {
   ElementRef,
   HostListener,
   HostBinding,
+  Input,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor!: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
