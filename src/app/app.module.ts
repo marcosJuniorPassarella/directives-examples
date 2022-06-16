ea6d897aced7f4fe52210da1d319d09d1c58309f
MJWORKS <mjworks2020@outlook.com>
2022-06-16 11:30:44 -0300

    Created a unless estructural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from '../shared/directives/basic-highlight.directive';
-import { BetterHighlightDirective } from '../shared/directives/better-highlight.directive';
+import { BasicHighlightDirective } from './shared/directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './shared/directives/better-highlight.directive';
+import { UnlessDirective } from './shared/directives/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
+    UnlessDirective,
   ],
   imports: [CommonModule, BrowserModule],
   providers: [],
